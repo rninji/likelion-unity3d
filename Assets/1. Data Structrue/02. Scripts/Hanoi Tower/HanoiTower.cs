ac601ccd661a73f1596b434613d7c3afccc8c971
Minji Kil <yellowminji@naver.com>
2025-07-11 23:34:14 +0900

    [ADD] 알고리즘 학습2
    25/07/11
    
    -- 알고리즘 학습 2
    > 하노이 타워 해결
    > 순차 탐색
    > 이진 탐색
    > 이진 탐색트리

--- Assets/1. Data Structrue/02. Scripts/Hanoi Tower/HanoiTower.cs
+++ Assets/1. Data Structrue/02. Scripts/Hanoi Tower/HanoiTower.cs
@@ -1,30 +1,43 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HanoiTower : MonoBehaviour
 {
     public enum HanoiLevel { Lv1 = 3, Lv2, Lv3 }
-
-    public HanoiLevel hanoiLevel;
+    public HanoiLevel hanoiLevel = HanoiLevel.Lv1;
 
     public GameObject[] donutPrefabs;
-    public BoardBar[] bars;
-    public static BoardBar currBar;
+    public BoardBar[] bars; // L, C, R
+
+    public TextMeshProUGUI countTextUI;
+    public Button answerButton;
 
     public static GameObject selectedDonut;
     public static bool isSelected;
+    public static BoardBar currBar;
+    public static int moveCount;
 
-    IEnumerator CreateDonuts()
+    void Awake()
     {
-        for (int i = (int)hanoiLevel -1; i >= 0; i--)
+        answerButton.onClick.AddListener(HanoiAnswer);
+    }
+    
+    IEnumerator Start()
+    {
+        for (int i = (int)hanoiLevel - 1; i >= 0; i--) // 반복문으로 Level만큼 도넛 생성
         {
-            GameObject donut = Instantiate(donutPrefabs[i]);
-            donut.transform.position = new Vector3(-5f, 5f, 0);
+            GameObject donut = Instantiate(donutPrefabs[i]); // 도넛 생성
+            donut.transform.position = new Vector3(-5f, 5f, 0); // 도넛 생성 위치 : 왼쪽 막대기 + 위쪽
 
-            bars[0].PushDonut(donut);
-            yield return new WaitForSeconds(0.5f);
+            bars[0].PushDonut(donut); // 방금 생성한 도넛을 해당 Bar의 Stack Push
+            
+            yield return new WaitForSeconds(1f); // 순차적으로 생성
         }
-        
+
+        moveCount = 0;
+        countTextUI.text = moveCount.ToString();
     }
 
     void Update()
@@ -36,25 +49,25 @@ public class HanoiTower : MonoBehaviour
             isSelected = false;
             selectedDonut = null;
         }
-    }
-
-    void Start()
-    {
-        StartCoroutine(CreateDonuts()); // 도넛 생성
-        HanoiTowerFunc((int)hanoiLevel, 0, 1, 2); // 해결 재귀 함수
-    }
-
-    public void HanoiTowerFunc(int n, int from, int temp, int to)
-    {
-        if (n == 0) return;
         
+        countTextUI.text = moveCount.ToString();
+    }
+    
+    public void HanoiAnswer()
+    {
+        HanoiRoutine((int)hanoiLevel, 0, 1, 2);
+    }
+    
+    private void HanoiRoutine(int n, int from, int temp, int to)
+    {
         if (n == 1)
-            Debug.Log($"{n}번 원반을 {from}에서 {to}로 이동");
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
         else
         {
-            HanoiTowerFunc(n-1, from, to, temp);
-            Debug.Log($"{n}번 원반을 {from}에서 {to}로 이동");
-            HanoiTowerFunc(n-1, from, to, temp);
+            HanoiRoutine(n - 1, from, to, temp);
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+            
+            HanoiRoutine(n - 1, temp, from, to);
         }
     }
 }
