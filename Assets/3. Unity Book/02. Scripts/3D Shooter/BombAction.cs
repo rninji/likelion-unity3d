90ac67a0d11b9ed5a593e3ef799ed4da2894cb01
Minji Kil <yellowminji@naver.com>
2025-07-25 13:41:28 +0900

    [UPDATE] 수류탄 데미지 적용
    25/07/25
    
    -- 수류탄
    > 프리팹 적용
    > 범위 내 Enemy 전부 공격

--- Assets/3. Unity Book/02. Scripts/3D Shooter/BombAction.cs
+++ Assets/3. Unity Book/02. Scripts/3D Shooter/BombAction.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 public class BombAction : MonoBehaviour
 {
     public GameObject bombEffect;
+
+    public int attackPower = 10;
+    private float explosionRadius = 5f;
     private void OnCollisionEnter(Collision other)
     {
+        Collider[] cols = Physics.OverlapSphere(transform.position, explosionRadius, 1 << 9);
+        
+        for (int i=0; i < cols.Length; i++)
+            cols[i].GetComponent<EnemyFSM>().HitEnemy(attackPower);
+        
         GameObject eff = Instantiate(bombEffect);
         eff.transform.position = transform.position;
         
