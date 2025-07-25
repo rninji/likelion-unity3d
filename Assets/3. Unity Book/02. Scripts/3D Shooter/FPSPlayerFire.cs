59d7ee2d8f4b9fa64d52d37b0237c3066e4044d4
Minji Kil <yellowminji@naver.com>
2025-07-25 16:44:42 +0900

    [UPDATE] Weapon Mode UI 이미지 수정
    25/07/25
    
    -- Weapon Mode UI
    > HP바 상단에 모드 이미지 추가
    > 모드 변경 시 텍스트와 함께 이미지도 변경
    > 모드 변경 시 크로스헤어 변경

--- Assets/3. Unity Book/02. Scripts/3D Shooter/FPSPlayerFire.cs
+++ Assets/3. Unity Book/02. Scripts/3D Shooter/FPSPlayerFire.cs
@@ -23,6 +23,15 @@ public class FPSPlayerFire : MonoBehaviour
 
     public GameObject[] eff_Flash;
 
+    public GameObject weapon01;
+    public GameObject weapon02;
+
+    public GameObject crosshair01;
+    public GameObject crosshair02;
+    
+    public GameObject weapon01_R;
+    public GameObject weapon02_R;
+
     void Start()
     {
         ps = bulletEffect.GetComponent<ParticleSystem>();
@@ -91,12 +100,25 @@ public class FPSPlayerFire : MonoBehaviour
             wMode = WeaponMode.Normal;
             Camera.main.fieldOfView = 60f;
             wModeText.text = "Normal Mode";
-
+            
+            weapon01.SetActive(true);
+            weapon02.SetActive(false);
+            crosshair01.SetActive(true);
+            crosshair02.SetActive(false);
+            weapon01_R.SetActive(true);
+            weapon02_R.SetActive(false);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             wMode = WeaponMode.Sniper;
             wModeText.text = "Sniper Mode";
+            
+            weapon01.SetActive(false);
+            weapon02.SetActive(true);
+            crosshair01.SetActive(false);
+            crosshair02.SetActive(true);
+            weapon01_R.SetActive(false);
+            weapon02_R.SetActive(true);
         }
     }
 
